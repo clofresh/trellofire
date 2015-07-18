34bb9f18326221bd7fde285786fdaca1f260cc70
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2015-07-18 13:50:32 -0400

    allow clicking anywhere in the row to click the checkbox

--- www/js/app.jsx
+++ www/js/app.jsx
@@ -45,13 +45,25 @@ define(['react', 'trello', 'query'], function(React, Trello, Query) {
   });
 
   var TrelloItem = React.createClass({
+    getInitialState: function() {
+      return {
+        checked: false
+      };
+    },
+    handleClick: function(event) {
+      if (event.target.tagName !== "A") {
+        this.setState({
+          checked: !this.state.checked
+        });
+      }
+    },
     render: function() {
       return (
-        <tr className="trelloItem">
+        <tr className="trelloItem" onClick={this.handleClick}>
           <td className="checkCol">
-            <input type="checkbox" />
+            <input type="checkbox" checked={this.state.checked} onChange={this.handleClick} />
           </td>
-          <td className="title;Col"><a href={this.props.data.url}>{this.props.data.name}</a></td>
+          <td className="titleCol"><a href={this.props.data.url}>{this.props.data.name}</a></td>
           <td className="listCol">{lists[this.props.data.idList].name}</td>
           <td className="stateCol">{this.props.data.closed ? "closed" : "open"}</td>
           <td className="dueCol">{this.props.data.due}</td>
